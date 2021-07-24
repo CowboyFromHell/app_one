a04ea34cec26bff324d2fd42b6255a094a2c1884
Antosha <cmex1966@gmail.com>
2021-07-24 23:01:40 +0300

    Test

--- source/app_py.py
+++ source/app_py.py
@@ -17,7 +17,7 @@ def query_example():
     #         return f"""Use this format: {{animal:"dog", sound:"woof", count: 4}}"""+"\n"
     #The root page
     res_text = f"""
-    Wake up Neo!<br/>
+    Hello!<br/>
     This is Python App!<br/>
     Made with 💖 by Antosha<br/>
     """
